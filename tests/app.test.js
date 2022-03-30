d8d8affc3be933eb0d7ab2c6d1de0e3eb9540a7b
Balach Subra <balachsubra@gmail.com>
2022-03-30 13:53:04 +0100

    moved test to folder

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,8 @@
 const { expect } = require("@jest/globals");
 const request = require("supertest");
-const app = require("./app");
+const app = require("../app");
 
-describe("Test the root path", () => {
+describe("Test the root path & If Hello world is being returned", () => {
     test("It should response the GET method", async () => {
       const response = await request(app).get("/");
       expect(response.statusCode).toBe(200);
