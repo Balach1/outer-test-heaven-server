f3a9e0de381da5ca05c40f3ab6ec00bcb382c080
Balach Subra <balachsubra@gmail.com>
2022-03-30 16:15:55 +0100

    trigger test

--- src/app.js
+++ src/app.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/t', (req, res) => {
+  res.send('Hello World!2')
+})
+
 module.exports = app;
--- tests/app.test.js
+++ tests/app.test.js
@@ -5,7 +5,10 @@ const app = require("../src/app");
 describe("Test the root path & If Hello world is being returned", () => {
     test("It should response the GET method", async () => {
       const response = await request(app).get("/");
+      const response2 = await request(app).get("/t");
+
       expect(response.statusCode).toBe(200);
       expect(response.text).toBe("Hello World!");
+      expect(response2.text).toBe("Hello World!2");
     });
   });
